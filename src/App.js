52d3a8a9d0c00bb19094ab6b1d1b91fe9041579f
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-26 14:41:57 +0200

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+
+      {/*Nav*/}
+
+      {/*Results*/}
+    </div>
+  );
 }
 
 export default App;

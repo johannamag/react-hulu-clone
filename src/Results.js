05987916cacb2646f43326257745771fcf206150
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-26 18:52:26 +0200

    Added FlipMove And  deployed on Firebase

--- src/Results.js
+++ src/Results.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Results.css";
 import VideoCard from "./VideoCard";
 import axios from "./axios";
-import requests from "./requests";
+import FlipMove from "react-flip-move";
 
 function Results({ selectedOption }) {
   const [movies, setMovies] = useState([]);
@@ -19,9 +19,11 @@ function Results({ selectedOption }) {
 
   return (
     <div className="results">
-      {movies.map((movie) => (
-        <VideoCard key={movie.id} movie={movie} />
-      ))}
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
     </div>
   );
 }

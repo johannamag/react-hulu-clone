711d7e4ae665bab213f99e5280def65c833b123f
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-26 18:05:23 +0200

    Added Results and VideoCard

---
+++ src/VideoCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./VideoCard.css";
+import TextTruncate from "react-text-truncate";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function VideoCard({ movie }) {
+  return (
+    <div className="videoCard">
+      <img
+        src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+        alt=""
+      />
+      <TextTruncate
+        line={1}
+        element="p"
+        truncateText="..."
+        text={movie.overview}
+      />
+      <TextTruncate
+        line={1}
+        element="h2"
+        truncateText="..."
+        text={movie.title || movie.original_name}
+      />
+      <p className="videoCard__stats">
+        {movie.release_date || movie.first_air_date} ·{" "}
+        <ThumbUpAltIcon className="thumb" /> {movie.vote_count}
+      </p>
+    </div>
+  );
+}
+
+export default VideoCard;
